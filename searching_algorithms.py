b42b6fdbc803ec1a792a54bd63d66c7b92750ac3
LeonardStefanut <steleonard2005@gmail.com>
2025-11-04 23:58:34 +0200

    ucs

--- searching_algorithms.py
+++ searching_algorithms.py
@@ -197,6 +197,56 @@ def astar(draw: callable, grid: Grid, start: Spot, end: Spot) -> bool:
             current.make_closed()
 
     return False
+
+def ucs(draw: callable, grid: Grid, start: Spot, end: Spot) -> bool:
+    if not start or not end:
+        return False
+
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start)) 
+
+    came_from: dict[Spot, Spot] = {}
+
+    g_score: dict[Spot, float] = {spot: float("inf") for row in grid.grid for spot in row}
+    g_score[start] = 0
+
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return False
+
+        current: Spot = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
+            start.make_start()
+            return True
+
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((g_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+
+        draw()
+
+        if current != start:
+            current.make_closed()
+
+    return False
 # and the others algorithms...
 # ▢ Depth-Limited Search (DLS)
 # ▢ Uninformed Cost Search (UCS)
